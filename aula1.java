9bce46fc4bf78ada66f008aa12d1a598b3483291
Fátima Pinheiro <fatimapinheir@gmail.com>
2021-05-13 10:58:48 -0300

    Create aula1.java

---
+++ aula1.java
@@ -0,0 +1,11 @@
+  public class Helloword{
+  //DeclaraÃ§Ã£o de variÃ¡veis
+  private String saudacao;
+  
+  //Construtor da classe
+      public Helloword (String n){
+          saudacao = n;
+         System.out.print(saudacao);
+      }
+ //Teste Commit Git
+  }
